7e940a8cd5ef65e70754e4936b3a707c6ff1763b
hoilamwong <142138307+hoilamwong@users.noreply.github.com>
2024-11-01 05:19:33 -0400

    init

--- script2.js
+++ script2.js
@@ -19,8 +19,8 @@ import { SSRPass } from 'three/examples/jsm/Addons.js'
 let camera, scene, renderer;
 let scene2, renderer2;
 
-let mouseX = 0,
-  mouseY = 0;
+let gltfScale = 100
+let domObjectScale = 0.43
 
 const sizes = {
   width: window.innerWidth,
@@ -28,22 +28,9 @@ const sizes = {
 }
 
 let controls, gui;
-
-let sphere;
-
-let lights = [];
-
 let effectComposer
-
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
-
 let lookAt = new THREE.Vector3(0, 0, 0);
-
-let lightMovementAmplitude = 200;
-
 init();
-animate(performance.now());
 
 function init() {
   gui = new GUI()
@@ -53,101 +40,18 @@ function init() {
   camera.position.set(0, 500, 1000);
 
   scene = new THREE.Scene();
-  console.log(scene);
   scene.background = new THREE.Color('black')
 
   scene2 = new THREE.Scene();
 
-  var element = document.createElement("iframe");
-  element.style.width = "300px";
-  element.style.height = "200px";
-  element.style.opacity = 0.999;
-  element.src = "http://192.168.1.16:5174/";
-
-  var domObject = new CSS3DObject(element);
-  var domObjectScale = 0.8
-  domObject.scale.set(domObjectScale, domObjectScale, 1)
-  domObject.rotation.set(0, 2.3, 0)
-  domObject.position.set(0, 100, 0)
-  scene2.add(domObject);
-
-
   /**
-   * Models
+   * Renderers
    */
-  const gltfLoader = new GLTFLoader()
-
-  gltfLoader.load(
-    '/models/pepe6.glb',
-    (gltf) => {
-      gltf.scene.traverse((object) => {
-
-        // console.log(object.name, object);
-        gltf.scene.position.y = -100
-        var gltfScale = 100
-        gltf.scene.scale.set(gltfScale, gltfScale, gltfScale)
-
-
-        if (object.name == 'Sun') {
-          console.log('found sun', object)
-          object.castShadow = true
-          object.shadow.bias = 3
-          object.shadow.normalBias = 0.3
-          object.intensity = 10
-        }
-        if (object.name == 'Sun001') {
-          console.log('found sun', object)
-          object.castShadow = false
-          object.shadow.bias = 0
-          object.shadow.normalBias = 0.3
-          object.intensity = 10
-        }
-        if (object.name == 'floor') {
-          const newFloor = new THREE.MeshMatcapMaterial()
-          object.material = newFloor
-          object.material.color = new THREE.Color('black')
-        }
-        if (object.name == 'monitor_retroscreen') {
-          console.log('found monitor_retroscreen', object)
-          object.material.emissiveIntensity = 3
-
-          
-          var material = new THREE.MeshBasicMaterial({
-            opacity: 0.2,
-            color: new THREE.Color("black"),
-            blending: THREE.NoBlending,
-            side: THREE.DoubleSide
-          });
-          var geometry = new THREE.PlaneGeometry(300, 200);
-          var mesh = new THREE.Mesh(geometry, material);
-          mesh.position.set(0, 10, 0);
-          mesh.position.copy(domObject.position);
-          mesh.rotation.copy(domObject.rotation);
-          mesh.scale.copy(domObject.scale);
-          mesh.castShadow = false;
-          mesh.receiveShadow = true;
-          scene.add(mesh);
-
-        }
-
-      }),
-        scene.add(gltf.scene),
-        updateAllMaterials()
-    }
-  )
-
-
-  //
-
   renderer2 = new CSS3DRenderer();
   renderer2.setSize(window.innerWidth, window.innerHeight);
   renderer2.domElement.style.position = "absolute";
   renderer2.domElement.style.top = 0;
   document.querySelector("#css").appendChild(renderer2.domElement);
-
-  let controls = new OrbitControls(camera, renderer2.domElement);
-  controls.target.set(0, 0, 0);
-  controls.update();
 
   renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setClearColor(0x000000, 0);
@@ -157,12 +61,132 @@ function init() {
   renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
   document.querySelector("#webgl").appendChild(renderer.domElement);
 
-  // let controls2 = new OrbitControls(camera, renderer.domElement);
-  // controls2.target.set(0, 0, 0);
-  // controls2.update();
+  controls = new OrbitControls(camera, renderer2.domElement);
+  controls.target.set(0, 0, 0);
+  controls.enableDamping = true
+  controls.dampingFactor = 0.05
+  controls.maxZoom = 10
+  controls.update();
 
-  document.addEventListener("mousemove", onDocumentMouseMove, false);
+  /**
+   * Cursor
+   */
+  const cursor = {}
+  cursor.x = 0
+  cursor.y = 0
 
+  window.addEventListener('mousemove', (event) => {
+    //normalized, -0.5 to 0.5
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+  })
+
+
+  /**
+   * Resize
+   */
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update effect composer
+    effectComposer.setSize(sizes.width, sizes.height)
+    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+
+  var element = document.createElement("iframe");
+  element.style.width = "650px";
+  element.style.height = "500px";
+  element.style.opacity = 1;
+  element.src = "http://192.168.1.16:5173/";
+
+  var domObject = new CSS3DObject(element);
+  domObject.scale.set(domObjectScale, domObjectScale, 1)
+  domObject.rotation.set(-3.141, 0.95 + 3.141, 3.141)
+  domObject.position.set(118.41244581647652, 92.79841251765995, -107.83197783392244)
+  // domObject.position.set(118, 200.79841251765995, -107.83197783392244)
+  scene2.add(domObject);
+
+  // document.addEventListener("mousemove", onDocumentMouseMove, false);
+
+  var material = new THREE.MeshStandardMaterial({
+    opacity: 0.2,
+    color: new THREE.Color("black"),
+    blending: THREE.NoBlending,
+    side: THREE.DoubleSide
+  });
+  material.emissive = true
+  material.emissiveIntensity = 100
+  var geometry = new THREE.PlaneGeometry(650, 500);
+  var screenFrame = new THREE.Mesh(geometry, material);
+  screenFrame.rotation.copy(domObject.rotation);
+  screenFrame.position.copy(domObject.position);
+  screenFrame.scale.copy(domObject.scale);
+  scene.add(screenFrame);
+
+  /**
+   * Models
+   */
+  const gltfLoader = new GLTFLoader()
+  gltfLoader.load(
+    '/models/pepe8.glb',
+    (gltf) => {
+      gltf.scene.traverse((object) => {
+
+        if (object.isMesh) {
+          object.material.envMapIntensity = 2.5
+          object.material.needsUpdate = true
+          object.castShadow = true
+          object.receiveShadow = true
+        }
+
+        gltf.scene.position.y = -100
+        gltf.scene.scale.set(gltfScale, gltfScale, gltfScale)
+
+
+        if (object.name == 'Sun') {
+          console.log('found sun', object)
+          object.castShadow = true
+          object.shadow.bias = 3
+          object.shadow.normalBias = 0.3
+          object.intensity = 10
+          gui.add(object, 'intensity').min(0).max(100).name('blue sun intensity')
+        }
+
+        if (object.name == 'Sun001') {
+          console.log('found sun2', object)
+          object.castShadow = false
+          object.shadow.bias = 0
+          object.shadow.normalBias = 0.3
+          object.intensity = 10
+          gui.add(object, 'intensity').min(0).max(100).name('green sun intensity')
+        }
+
+        if (object.name == 'floor') {
+          const newFloor = new THREE.MeshMatcapMaterial()
+          object.material = newFloor
+          object.material.color = new THREE.Color('black')
+        }
+
+        if (object.name == 'monitor_retroscreen') {
+          console.log('found monitor_retroscreen', object)
+          object.material.emissiveIntensity = 3
+        }
+
+      }),
+        scene.add(gltf.scene)
+    }
+  )
 
   /**
  * Post processing
@@ -223,12 +247,8 @@ function init() {
     width: window.innerWidth,
     height: window.innerHeight,
   });
-  console.log(bokehPass);
   bokehPass.enabled = false
   gui.add(bokehPass, 'enabled')
-  gui.add(bokehPass.uniforms.focus, 'value').min(-5000).max(500000).step(0.0001)
-  gui.add(bokehPass.uniforms.aperture, 'value').min(-1).max(1).step(0.001)
-  gui.add(bokehPass.uniforms.maxblur, 'value').min(0).max(0.5).step(0.0001)
 
 
   effectComposer.addPass(ssrPass)
@@ -237,27 +257,25 @@ function init() {
 }
 
 function onDocumentMouseMove(event) {
-  mouseX = event.clientX - windowHalfX;
-  mouseY = event.clientY - windowHalfY;
+  // mouseX = event.clientX - windowHalfX;
+  // mouseY = event.clientY - windowHalfY;
 }
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
 
-const updateAllMaterials = () => {
-  scene.traverse((child) => {
-    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
-      child.material.envMapIntensity = 2.5
-      child.material.needsUpdate = true
-      child.castShadow = true
-      child.receiveShadow = true
-    }
-  })
-}
-
-function animate(time) {
+const tick = () => {
 
   camera.lookAt(lookAt);
+
+  controls.update();
 
   effectComposer.render()
   renderer2.render(scene2, camera);
 
-  requestAnimationFrame(animate);
+  window.requestAnimationFrame(tick);
 }
+
+tick()
